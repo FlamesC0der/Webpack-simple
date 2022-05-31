60619aea3f1723a596668ad5e0dbd066445c5091
FlameCoder0_0 <73310806+Alexey178@users.noreply.github.com>
2022-06-01 02:01:19 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import './webpack-logo.png'
+import './styles/styles.css'
+import './styles/style.scss'
+console.log('test')
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { AutomaticPrefetchPlugin } = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader
+        },
+        'css-loader',
+    ]
+    if (extra) {
+        loaders.push(extra)
+    }
+    return loaders
+}
+
+const plugins = () => {
+    const base = [
+        new HtmlWebpackPlugin({
+            title: 'Webpack FlameCoder0_0',
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('.css')
+        })
+    ]
+
+    return base
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    devServer: {
+        open: true,
+        hot: true,
+        port: 4200,
+        static: {
+            directory: './src',
+            watch: true
+        }
+    },
+    devtool: isDev ? 'source-map': false,
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: 'asset/resource'
+            }
+        ]
+    }
+}
